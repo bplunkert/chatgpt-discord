ef52db8221aa67e99408095fcad8b5e3e4f1cfa2
Ben Plunkert <bplunkert@Bens-MBP.hsd1.pa.comcast.net>
2022-12-06 13:29:34 -0500

    add chatgpt back in

--- app.py
+++ app.py
@@ -9,43 +9,57 @@ if not 'DISCORD_TOKEN' in os.environ:
 
 if not 'OPENAI_API_KEY' in os.environ:
   raise Exception('Environment variable OPENAI_API_KEY is required')
+if 'CHATGPT_SESSION_TOKEN' in os.environ:
+  config = {
+    'Authorization': '<API-KEY>',
+    'session_token': os.environ['CHATGPT_SESSION_TOKEN']
+  }
 else:
-  openai.api_key = os.getenv("OPENAI_API_KEY")
+  if 'CHATGPT_EMAIL' in os.environ:
+    if 'CHATGPT_PASSWORD' in os.environ:
+      config = {
+        'email': os.environ['CHATGPT_EMAIL'],
+        'password': os.environ['CHATGPT_PASSWORD']
+      }
+    else:
+      raise Exception('CHATGPT_EMAIL requires CHATGPT_PASSWORD')
+  else:
+    raise Exception('ChatGPT requires credentials in environment variables: either CHATGPT_SESSION_TOKEN or a pair of CHATGPT_EMAIL and CHATGPT_PASSWORD')
+
+ai = Chatbot(config, conversation_id=None)
+ai.refresh_session()
 
 class ChatBot(discord.Client):
   async def on_message(self, message):
-    # don't respond to ourselves
-    if message.author == self.user:
-        return
-    
-    # if message.content == 'reset':
-    #   ai.reset_chat()
-    #   await message.channel.send("Reset AI conversation.")
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        
+        if message.content == 'reset':
+          ai.reset_chat()
+          await message.channel.send("Reset AI conversation.")
+        else:
+          input_prompt = message.content
+          if input_prompt.startswith('meme'):
+            response = openai.Image.create(
+              prompt=input_prompt,
+              n=1,
+              size="256x256"
+            )
+            image_url = response['data'][0]['url']
+            await self.send_discord_message(message, image_url)
+          else:
+            prompt = message.content
+            resp = ai.get_chat_response(prompt, output="text")
+            await self.send_discord_message(message, resp['message'])
 
-    else:
-      input_prompt = message.content
-
-      if input_prompt.startswith('meme'):
-        response = openai.Image.create(
-          prompt=input_prompt,
-          n=1,
-          size="256x256"
-        )
-        image_url = response['data'][0]['url']
-        await self.send_discord_message(message, image_url)
-      else:
-        response = openai.Completion.create(
-          model="text-davinci-002",
-          prompt="Jarvis: I am a scholar, well educated in all subjects, but especially in Shakespeare. You: Who is the author of King Lear? Jarvis: Why Shakespeare, of course.",
-          temperature=0.9,
-          max_tokens=60,
-          top_p=1.0,
-          frequency_penalty=0.5,
-          presence_penalty=0.0,
-          stop=[f"You: {input_prompt}"]
-        ).choices[0].text
-
-      await self.send_discord_message(message, response)
+            response = openai.Image.create(
+              prompt=resp['message'],
+              n=1,
+              size="256x256"
+            )
+            image_url = response['data'][0]['url']
+            await self.send_discord_message(message, image_url)
 
   async def send_discord_message(self, message, msg):
     import textwrap
