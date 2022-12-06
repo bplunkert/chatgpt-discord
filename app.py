1efc648aae803b73c8d29f2dcff5cd442b463752
Ben Plunkert <bplunkert@Bens-MBP.hsd1.pa.comcast.net>
2022-12-06 12:30:51 -0500

    meme works

--- app.py
+++ app.py
@@ -14,27 +14,38 @@ else:
 
 class ChatBot(discord.Client):
   async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-        
-        # if message.content == 'reset':
-        #   ai.reset_chat()
-        #   await message.channel.send("Reset AI conversation.")
+    # don't respond to ourselves
+    if message.author == self.user:
+        return
+    
+    # if message.content == 'reset':
+    #   ai.reset_chat()
+    #   await message.channel.send("Reset AI conversation.")
 
-        else:
-          prompt = message.content
-          response = openai.Completion.create(
-            model="code-davinci-002",
-            prompt="You: How do I combine arrays?\nJavaScript chatbot: You can use the concat() method.\nYou: How do you make an alert appear after 10 seconds?\nJavaScript chatbot",
-            temperature=0,
-            max_tokens=60,
-            top_p=1.0,
-            frequency_penalty=0.5,
-            presence_penalty=0.0,
-            stop=["You:"]
-          ).choices[0].text
-          await self.send_discord_message(message, response)
+    else:
+      input_prompt = message.content
+
+      if input_prompt.startswith('meme'):
+        response = openai.Image.create(
+          prompt=input_prompt,
+          n=1,
+          size="256x256"
+        )
+        image_url = response['data'][0]['url']
+        await self.send_discord_message(message, image_url)
+      else:
+        response = openai.Completion.create(
+          model="text-davinci-002",
+          prompt="Jarvis: I am a scholar, well educated in all subjects, but especially in Shakespeare. You: Who is the author of King Lear? Jarvis: Why Shakespeare, of course.",
+          temperature=0.9,
+          max_tokens=60,
+          top_p=1.0,
+          frequency_penalty=0.5,
+          presence_penalty=0.0,
+          stop=[f"You: {input_prompt}"]
+        ).choices[0].text
+
+      await self.send_discord_message(message, response)
 
   async def send_discord_message(self, message, msg):
     import textwrap
